6f75d48d26a30b99e426ee6f4e05760f561d2bcd
M Farkas-Dyck <strake888@gmail.com>
2021-01-12 14:28:46 -0800

    Optionally use `core::intrinsics::abort`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,27 @@
 #![no_std]
 
+#![cfg_attr(feature = "nightly", feature(core_intrinsics))]
+
 //! Abnormal termination: see [`abort`].
-
-struct A;
-
-impl Drop for A {
-    #[inline(always)]
-    fn drop(&mut self) { panic!() }
-}
 
 /// Abnormally terminate the process.
 #[inline(always)]
-pub fn abort() -> ! {
+pub fn abort() -> ! { a() }
+
+#[cfg(feature = "nightly")]
+#[inline(always)]
+fn a() -> ! { core::intrinsics::abort() }
+
+#[cfg(not(feature = "nightly"))]
+#[inline(always)]
+fn a() -> ! {
+    struct A;
+
+    impl Drop for A {
+        #[inline(always)]
+        fn drop(&mut self) { panic!() }
+    }
+
     let _a = A;
     panic!()
 }
