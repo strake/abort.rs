785c543ab3a4e34b976cefd666623abf86dcf6bf
M Farkas-Dyck <strake888@gmail.com>
2021-01-12 10:26:55 -0800

    Not depend on C compiler/library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 #![no_std]
 
-extern "C" {
-    #[link_name = "abort"]
-    fn c_abort() -> !;
+//! Abnormal termination: see [`abort`].
+
+struct A;
+
+impl Drop for A {
+    #[inline(always)]
+    fn drop(&mut self) { panic!() }
 }
 
+/// Abnormally terminate the process.
 #[inline(always)]
-pub fn abort() -> ! { unsafe { c_abort() } }
+pub fn abort() -> ! {
+    let _a = A;
+    panic!()
+}
